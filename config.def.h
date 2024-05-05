f1b7be5d77d12b49fb8f937ae9fbe85a604c8d08
elohim121 <facebook12@gmail.com>
2024-05-05 23:34:54 +0000

    add key for blueman-manager

--- config.def.h
+++ config.def.h
@@ -62,6 +62,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lock[] = { "slock", NULL };
+static const char *blue[] = { "blueman-manager", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -69,6 +70,7 @@ static const Key keys[] = {
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY, 	                XK_BackSpace, spawn,       {.v = lock } },
 	{ MODKEY,	                XK_w,      spawn,          {.v = (const char* []) { BROWSER, NULL } } },
+	{ MODKEY,	                XK_F2,     spawn,          {.v = blue } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
